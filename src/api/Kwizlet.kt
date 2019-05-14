fd7a56908266e538353f0bbe63599eacb00fc192
PotatoCurry <potatocurry@protonmail.com>
2019-05-14 08:19:34 -0500

    Add password-protected set option

--- src/api/Kwizlet.kt
+++ src/api/Kwizlet.kt
@@ -3,7 +3,7 @@ package io.github.potatocurry.kwizlet.api
 internal const val quizletEndpoint = "https://api.quizlet.com/2.0/"
 
 class Kwizlet(private val clientID: String) {
-    fun getSet(setID: String): Set {
-        return Set(clientID, setID)
+    fun getSet(setID: String, password: String? = null): Set {
+        return Set(clientID, setID, password)
     }
 }
