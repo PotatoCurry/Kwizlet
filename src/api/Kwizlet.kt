2ce488c9e3f34bc71ad6479828c967ea46b75db5
PotatoCurry <potatocurry@protonmail.com>
2019-05-21 08:40:38 -0500

    Add parseURL

--- src/api/Kwizlet.kt
+++ src/api/Kwizlet.kt
@@ -1,9 +1,21 @@
 package io.github.potatocurry.kwizlet.api
 
+import java.net.URL
+
 internal const val quizletEndpoint = "https://api.quizlet.com/2.0"
 
 class Kwizlet(private val clientID: String) {
+    /** Returns a [Set] by [setID] with an optional [password]. */
     fun getSet(setID: String, password: String? = null): Set {
         return Set(clientID, setID, password)
+    }
+
+    /**
+     * Returns the ID of the [Set] the [url] refers to.
+     * Often coupled with [getSet].
+     * */
+    fun parseURL(url: String): String {
+        val path = URL(url).path.split("/")
+        return path.first(String::isNotEmpty)
     }
 }
